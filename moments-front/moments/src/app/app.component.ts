5b3978bc8a3288eca56da608cada827faac141dd
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-19 10:37:02 -0300

    adicionado imports, criado os services e interfaces para momment , comment e messages

--- moments-front/moments/src/app/app.component.ts
+++ moments-front/moments/src/app/app.component.ts
@@ -4,21 +4,28 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppRoutingModule } from './app.routes';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true, // Adicione esta linha
-  imports: [AppRoutingModule, CommonModule, RouterOutlet, HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [
+    AppRoutingModule, 
+    CommonModule, 
+    RouterOutlet, 
+    HeaderComponent, 
+    FooterComponent, 
+    RouterOutlet,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
-
 })
 
 export class AppComponent {
   title = 'moments';
-
 }
