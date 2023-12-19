e03a37d889d8a0bdb7d4a941aa274c8a619f3b95
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-19 12:06:45 -0300

    avalidação basica dos input de form e config para o form ser reativo

--- moments-front/moments/src/app/components/moment-form/moment-form.component.ts
+++ moments-front/moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,13 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 export class MomentFormComponent {
   @Input() btnText!: string
+
+  momentForm!: FormGroup;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl(''),
+    })
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log('enviou formulario');
+    
+  }
 
 }
