8aa3e6d91e5edb0b6a2e401edb2e94d1a4a1abc9
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-20 17:30:23 -0300

    criaçao do delete moment na service e chamado no moment.component

--- moments-front/moments/src/app/components/pages/moment/moment.component.ts
+++ moments-front/moments/src/app/components/pages/moment/moment.component.ts
@@ -6,6 +6,8 @@ import { CommonModule } from '@angular/common';
 
 import { MomentService } from '../../../services/moment.service';
 
+import { MessagesService } from '../../../services/messages.service';
+
 import { Moment } from '../../../Moment';
 
 import { environment } from '../../../../environments/environments';
@@ -28,7 +30,12 @@ export class MomentComponent implements OnInit {
   faTimes = faTimes;
   faEdit = faEdit;
 
-  constructor(private momentService: MomentService, private route: ActivatedRoute) { }
+  constructor(
+    private momentService: MomentService, 
+    private messagesService: MessagesService , 
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
  
@@ -36,5 +43,13 @@ export class MomentComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getMoment(id).subscribe((item) => this.moment = item.data)
+  }
+
+  async removeHandler(id: number) {
+    await this.momentService.removeMoment(id).subscribe()
+
+    this.messagesService.add('Momento excluido com sucesso!')
+
+    this.router.navigate(['/']);
   }
 }
