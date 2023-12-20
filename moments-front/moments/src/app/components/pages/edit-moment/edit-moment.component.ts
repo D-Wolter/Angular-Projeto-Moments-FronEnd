8cfc33c334e16815784f095f0f1decc137804a70
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-20 17:46:27 -0300

    craido componente edit-moment

---
+++ moments-front/moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-moment',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-moment.component.html',
+  styleUrl: './edit-moment.component.css'
+})
+export class EditMomentComponent {
+
+}
