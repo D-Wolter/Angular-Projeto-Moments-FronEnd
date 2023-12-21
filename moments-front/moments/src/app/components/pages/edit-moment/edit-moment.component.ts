c3be53efc23d982ffa0e593d5d186fe8a2916522
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-20 23:42:15 -0300

    iniciando edit-moment getById ok

--- moments-front/moments/src/app/components/pages/edit-moment/edit-moment.component.ts
+++ moments-front/moments/src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+
+import { Moment } from '../../../Moment';
+
+import { MomentService } from '../../../services/moment.service';
 
 @Component({
   selector: 'app-edit-moment',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './edit-moment.component.html',
   styleUrl: './edit-moment.component.css'
 })
-export class EditMomentComponent {
+export class EditMomentComponent implements OnInit{
+  moment?: Moment;
+  btnText: string = 'editar';
 
+  constructor(
+    private momentService: MomentService,
+    private route: ActivatedRoute,
+    ) { }
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.momentService.getMoment(id).subscribe((item) => this.moment = item.data)
+  }
 }
