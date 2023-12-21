c7d1de3250d7a434389b0075297b663a89c3b239
Daniel Wolter <wolter.daniel@gmail.com>
2023-12-21 13:50:36 -0300

    criando post de comentarios, base back-end no padrao da interface p front

--- moments-front/moments/src/app/components/pages/moment/moment.component.ts
+++ moments-front/moments/src/app/components/pages/moment/moment.component.ts
@@ -1,19 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router, ActivatedRoute, RouterModule } from '@angular/router';
-
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, Validators, FormGroupDirective  } from '@angular/forms';
 
 import { MomentService } from '../../../services/moment.service';
-
 import { MessagesService } from '../../../services/messages.service';
+import { CommentService } from '../../../services/comment.service';
 
 import { Moment } from '../../../Moment';
+import { Coment } from '../../../Coment';
 
 import { environment } from '../../../../environments/environments';
 
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons';
-
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
@@ -30,9 +29,12 @@ export class MomentComponent implements OnInit {
   faTimes = faTimes;
   faEdit = faEdit;
 
+  commentForm!: FormGroup;
+
   constructor(
     private momentService: MomentService, 
-    private messagesService: MessagesService , 
+    private messagesService: MessagesService,
+    private commentService: CommentService,
     private route: ActivatedRoute,
     private router: Router
     ) { }
@@ -42,7 +44,16 @@ export class MomentComponent implements OnInit {
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.momentService.getMoment(id).subscribe((item) => (this.moment = item.data))
+
+    this.commentForm = new FormGroup({
+      text: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    })
   }
+
+  get text() { return this.commentForm.get('text')!; }
+
+  get username() { return this.commentForm.get('username')!; }
 
   async removeHandler(id: number) {
     await this.momentService.removeMoment(id).subscribe()
